b1e7c0e4280821cadeb6160a324eea0a51c01289
Ashmeet <ashmeetk123@gmail.com>
2020-08-09 01:56:23 +0530

    commit

---
+++ client/src/components/Ash2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Ash2 = props => {
+  return (
+    <div>
+      <h1>this is Ashmeet</h1>
+      <p>you are here for {props.Time}</p>
+    </div>
+  );
+};
+
+export default Ash2;
